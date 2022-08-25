0df38505acf33620c231de887cbc16b7a010a953
edegp <a.yuhi1164@gmail.com>
2022-08-26 00:15:43 +0900

    ローディングの描画をレイアウトに移動

--- front/components/Layout.tsx
+++ front/components/Layout.tsx
@@ -1,12 +1,19 @@
 import React, { ReactNode, useState } from "react";
 import ErrorModal from "components/ErrorModal";
 import { useSelector } from "react-redux";
-import { RootState } from "store";
+import { Box, Loader } from "@mantine/core";
+import { State } from "types";
 
 export default function Layout({ children }: { children: ReactNode }) {
   // eslint-disable-next-line react-redux/useSelector-prefer-selectors
-  const lineUser = useSelector((state: RootState) => state.lineUser);
+  const { lineUser, isLoading } = useSelector((state: State) => state);
   const wrap = lineUser ? "wrap" : "hidden";
+  if (isLoading)
+    return (
+      <Box className="fixed inset-1/2">
+        <Loader variant="bars" color="green.6" />
+      </Box>
+    );
   return (
     <div className={wrap}>
       {children}
