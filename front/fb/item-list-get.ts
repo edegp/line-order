72efed543d161d8f0b91e43d73b20056d742b3aa
edegp <a.yuhi1164@gmail.com>
2022-09-01 22:49:20 +0900

    いらないファイルの削除

--- front/fb/item-list-get.ts
+++
@@ -1,4 +0,0 @@
-import { getDocs } from "firebase/firestore/lite";
-import { collection, query, where } from "firebase/firestore";
-import { db } from "./firebase-client";
-import * as functions from "firebase-fucntions";
