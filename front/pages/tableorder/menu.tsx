1482cc9b73b5fbe8d10d24002c369d7ff7f00c87
edegp <a.yuhi1164@gmail.com>
2022-09-01 22:55:47 +0900

    usecallbackで関数最適化　レイアウト調整

--- front/pages/tableorder/menu.tsx
+++ front/pages/tableorder/menu.tsx
@@ -3,9 +3,7 @@ import {
   Avatar,
   Box,
   Button,
-  Card,
   Container,
-  Dialog,
   Drawer,
   Footer,
   Grid,
@@ -17,10 +15,15 @@ import {
   Space,
   Text,
   Title,
+  Divider,
+  TextInput,
+  Slider,
+  Dialog,
+  Popover,
 } from "@mantine/core";
 import { CountdownTimerIcon, StarFilledIcon } from "@radix-ui/react-icons";
 import MenuCard from "components/tableorder/MenuCard";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { setCustomer, setOrders, setPaymentId } from "store";
 import { MenuType, State, Category } from "types";
 import { ocopy } from "utils/helper";
@@ -38,6 +41,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import Basket from "components/tableorder/basket";
+import { db } from "fb/firebase-client";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 
 export default function SeatNo(props: any) {
   let {
@@ -67,41 +72,52 @@ export default function SeatNo(props: any) {
     target: null as any,
     order: {} as MenuType,
   });
+  const [confirm, setConfirm] = useState(false);
   const [basketDrower, setBasketDrower] = useState(false);
-  const openDialog = (order: any) => {
-    const { itemId, categoryName, price, discountWay } = order;
-    setAddToBasket({
-      ...addToBasket,
-      itemId,
-      category: categoryName,
-      number: 1,
-      discountedPrice: price - TableOrder().utils.getDiscountPrice(order),
-      isDiscounted: Boolean(discountWay !== 0),
-      order: order,
-    });
-    setMenuDialog(true);
-  };
-  const avatar = (category: Category) =>
-    category.categoryId === 0 || category.categoryId === 10 ? (
-      <StarFilledIcon />
-    ) : category.categoryId === 1 || category.categoryId === 11 ? (
-      <MdLocalBar />
-    ) : category.categoryId === 2 || category.categoryId === 12 ? (
-      <FaRunning />
-    ) : category.categoryId === 3 || category.categoryId === 13 ? (
-      <FaUtensils />
-    ) : (
-      (category.categoryId === 5 || category.categoryId === 15) ?? (
-        <CountdownTimerIcon />
-      )
-    );
-  const search = async (categoryId: number, categoryName: string) => {
-    setCategoryName(categoryName);
-    const itemList = (await TableOrder().getItemData(categoryId))?.data;
-    setMenuList(itemList?.items);
-    setCategoryDialog(false);
-  };
-  const countHandler = () => {
+  const openDialog = useCallback(
+    (order: any) => {
+      const { itemId, categoryName, price, discountWay } = order;
+      setAddToBasket({
+        ...addToBasket,
+        itemId,
+        category: categoryName,
+        number: 1,
+        discountedPrice: price - TableOrder().utils.getDiscountPrice(order),
+        isDiscounted: Boolean(discountWay !== 0),
+        order: order,
+      });
+      setMenuDialog(true);
+    },
+    [addToBasket]
+  );
+  const avatar = useCallback(
+    (category: Category) =>
+      category.categoryId === 0 || category.categoryId === 10 ? (
+        <StarFilledIcon />
+      ) : category.categoryId === 1 || category.categoryId === 11 ? (
+        <MdLocalBar />
+      ) : category.categoryId === 2 || category.categoryId === 12 ? (
+        <FaRunning />
+      ) : category.categoryId === 3 || category.categoryId === 13 ? (
+        <FaUtensils />
+      ) : (
+        (category.categoryId === 5 || category.categoryId === 15) ?? (
+          <CountdownTimerIcon />
+        )
+      ),
+    []
+  );
+  const search = useCallback(
+    async (categoryId: number, categoryName: string) => {
+      setCategoryName(categoryName);
+      const docRef = doc(db, "TableOrderItemList", categoryId.toString());
+      const itemList: any = (await getDoc(docRef))?.data();
+      setMenuList(itemList?.items);
+      setCategoryDialog(false);
+    },
+    []
+  );
+  const countHandler = useCallback(() => {
     //バスケット内の個数
     let ret = 0;
     if (orders) {
@@ -113,8 +129,8 @@ export default function SeatNo(props: any) {
       }
     }
     setCount(ret);
-  };
-  const handleOrder = () => {
+  }, [orders]);
+  const handleOrder = useCallback(async () => {
     const categoryId = addToBasket.order.categoryId;
     const itemId = addToBasket.itemId;
     const price = addToBasket.isDiscounted
@@ -152,6 +168,9 @@ export default function SeatNo(props: any) {
       };
     }
     dispatch(setOrders({ ...orders, ...ordersObject }));
+
+    setMenuDialog(false);
+    await new Promise(() => setTimeout(() => {}, 100));
     // Clear
     setAddToBasket({
       category: null,
@@ -164,8 +183,7 @@ export default function SeatNo(props: any) {
       order: {} as MenuType,
     });
     // Close Dialog
-    setMenuDialog(false);
-  };
+  }, [orders, addToBasket]);
   useEffect(() => {
     countHandler();
   }, [orders]);
@@ -180,6 +198,7 @@ export default function SeatNo(props: any) {
         setCustomer({ ...customer, seatNo: parseInt(router.query?.seatNo, 10) })
       );
     } else {
+      dispatch(setCustomer({ ...customer, seatNo: 1 }));
       setSeatNoModal(true);
     }
     // paymentIdが存在しない場合には取得を試みる
@@ -194,20 +213,26 @@ export default function SeatNo(props: any) {
   return (
     <AppShell
       header={
-        <Header height={70}>
-          <Grid className="mt-[-2px]" justify="space-around" align="center">
-            <Grid.Col span={1}>
-              <Menu closeOnClickOutside={true} position="bottom-start">
+        <Header className='flex' height={70}>
+          <Grid
+            className='self-center w-full'
+            justify='space-evenly'
+            align='center'
+          >
+            <Grid.Col span={1} offset={1}>
+              <Menu closeOnClickOutside={true} position='bottom-start'>
                 <Menu.Target>
                   <Button
-                    color="dark"
-                    variant="subtle"
-                    className="hover:bg-slate-400 hover:rounded-3xl self-center justify-self-center text-md ml-6 p-2 w-[36px]"
+                    color='dark'
+                    variant='subtle'
+                    className='hover:bg-slate-400 hover:rounded-3xl self-center justify-self-center text-md 
+                    
+                    p-2 w-[36px]'
                   >
-                    <MdMenu className="text-slate-500" />
+                    <MdMenu className='text-slate-500' />
                   </Button>
                 </Menu.Target>
-                <Menu.Dropdown className="top-[70px]">
+                <Menu.Dropdown className='top-[70px]'>
                   <Menu.Label>Menu Category</Menu.Label>
                   {categoryList.map((category: Category, index: number) => (
                     <Menu.Item
@@ -223,15 +248,15 @@ export default function SeatNo(props: any) {
                 </Menu.Dropdown>
               </Menu>
             </Grid.Col>
-            <Grid.Col span={7}>
-              <Group className="self-center" position="center" align="center">
+            <Grid.Col span={4} sm={6}>
+              <Group className='self-center' position='center' align='center'>
                 <Avatar
-                  className="hidden sp:block"
-                  size="lg"
+                  className='hidden sp:block'
+                  size='lg'
                   src={lineUser.image}
                   alt={`${lineUser.name} 様`}
                 />
-                <Text className="hidden sp:block">
+                <Text className='hidden sp:block'>
                   {t?.menu.msg001.replace("{name}", customer.name)}
                 </Text>
                 <Text>
@@ -239,11 +264,11 @@ export default function SeatNo(props: any) {
                 </Text>
               </Group>
             </Grid.Col>
-            <Grid.Col span={2}>
+            <Grid.Col span={5} sm={2}>
               <Button
                 disabled={!paymentId}
-                className="text-gray bg-transparent"
-                rightIcon={<MdKeyboardArrowRight className="text-md" />}
+                className='text-gray bg-transparent'
+                rightIcon={<MdKeyboardArrowRight className='text-md' />}
               >
                 {t?.menu.msg003}
               </Button>
@@ -252,10 +277,10 @@ export default function SeatNo(props: any) {
         </Header>
       }
       footer={
-        <Footer fixed className="p-0" height={60}>
+        <Footer fixed className='p-0' height={60}>
           <Button
             fullWidth
-            className="bg-line text-white w-full h-full hover:bg-line/70"
+            className='bg-line text-white w-full h-full hover:bg-line/70 active:bg-line/40'
             onClick={() => setBasketDrower(true)}
             disabled={count === 0}
           >
@@ -287,36 +312,37 @@ export default function SeatNo(props: any) {
         opened={menuDialog}
         withCloseButton
         onClose={() => setMenuDialog(false)}
-        size="lg"
-        radius="sm"
-        target=".mantine-Container-root"
+        size='lg'
+        radius='sm'
+        transitionDuration={200}
+        target='.mantine-Container-root'
       >
         {addToBasket.order && (
-          <Box className="h-[350px]">
-            <Group className="h-[150px] w-[240px] mx-auto">
-              <Box className="mx-auto my-0 relative w-full h-[130px] max-w-[240px]">
+          <Box className='h-[350px]'>
+            <Group className='h-[150px] w-[240px] mx-auto'>
+              <Box className='mx-auto my-0 relative w-full h-[130px] max-w-[240px]'>
                 <Image
                   src={addToBasket.order?.imageUrl}
-                  className="absolute t-0 left-0"
-                  layout="fill"
-                  objectFit="contain"
+                  className='absolute t-0 left-0'
+                  layout='fill'
+                  objectFit='contain'
                 />
               </Box>
             </Group>
-            <Title align="center" className="pt-2 pb-0">
+            <Title align='center' className='pt-2 pb-0'>
               {addToBasket.order?.itemName}
             </Title>
-            <Text align="center" color="gray" className="text-sm pt-1">
+            <Text align='center' color='gray' className='text-sm pt-1'>
               {addToBasket.order?.itemDespription}
             </Text>
-            <Space h="md" />
-            <Group align="center" position="center" noWrap>
+            <Space h='md' />
+            <Group align='center' position='center' noWrap>
               {addToBasket.isDiscounted ? (
-                <Text size="sm" className="relative">
-                  <span className="absolute top-[-10px] left-[-35px] text-white opacity-90 z-30 rounded-2xl text-xs px-2 bg-red-500 ">
+                <Text size='sm' className='relative'>
+                  <span className='absolute top-[-12px] left-[calc(-26px-1vw)] text-white opacity-90 z-30 rounded-2xl text-xs px-2 bg-red-500 '>
                     SALE
                   </span>
-                  <span className="text-red-500 text-md font-bold">
+                  <span className='text-red-500 text-md font-bold'>
                     {t?.menucard.yen.replace(
                       "{price}",
                       addToBasket.discountedPrice?.toLocaleString()
@@ -324,7 +350,7 @@ export default function SeatNo(props: any) {
                   </span>
                 </Text>
               ) : (
-                <Text size="md">
+                <Text size='md'>
                   {t?.menucard.yen.replace(
                     "{price}",
                     addToBasket.order?.price?.toLocaleString()
@@ -334,34 +360,96 @@ export default function SeatNo(props: any) {
               <MdClose />
               <NumberInput
                 defaultValue={1}
-                className="max-w-[160px]"
+                min={1}
+                className='max-w-[160px]'
                 onChange={(number) =>
                   setAddToBasket({ ...addToBasket, number: number || 0 })
                 }
               />
-              <span className="text-nuatal-500 text-sm">個</span>
+              <span className='text-nuatal-500 text-sm'>個</span>
             </Group>
           </Box>
         )}
         <Button
           fullWidth
-          className="absolute left-0 bottom-0 border-line bg-line rounded-none rounded-br-sm rounded-bl-sm hover:bg-line/70"
+          className='absolute left-0 bottom-0 border-line bg-line rounded-none rounded-br-sm rounded-bl-sm hover:bg-line/70 active:bg-line/40'
           onClick={handleOrder}
-          rightIcon={<MdAddShoppingCart className="inline text-sm" />}
+          rightIcon={<MdAddShoppingCart className='inline text-sm' />}
         >
           {t?.menu.msg008}
         </Button>
       </Modal>
+      <Modal
+        opened={seatNoModal}
+        onClose={() => setSeatNoModal(false)}
+        title={t.menu.msg009}
+        closeOnClickOutside={false}
+        centered
+      >
+        <Slider
+          label={`座席番号 ${customer.seatNo}`}
+          labelAlwaysOn
+          min={1}
+          max={30}
+          value={customer.seatNo}
+          onChange={(value) =>
+            dispatch(setCustomer({ ...customer, seatNo: value }))
+          }
+          step={1}
+          mr={3}
+          mt={3}
+        />
+        <Popover width={200} position='top-end' withArrow shadow='md'>
+          <Popover.Target>
+            <Box className={"text-right"}>
+              <Button
+                className='justify-self-end self-end'
+                mt={20}
+                onClick={() => setConfirm(true)}
+              >
+                確認
+              </Button>
+            </Box>
+          </Popover.Target>
+          <Popover.Dropdown>
+            <Text>確認</Text>
+            <Text align='center'>
+              本当に
+              <br />
+              座席番号：{customer.seatNo} 番
+              <br />
+              で間違いないですか？
+            </Text>
+            <Button
+              mt={10}
+              onClick={() => {
+                setSeatNoModal(false);
+                setConfirm(false);
+                if (!customer.seatNo) {
+                  dispatch(dispatch(setCustomer({ ...customer, seatNo: 1 })));
+                }
+              }}
+            >
+              OK
+            </Button>
+          </Popover.Dropdown>
+        </Popover>
+      </Modal>
       <Drawer
+        lockScroll
         opened={basketDrower}
         onClose={() => setBasketDrower(false)}
-        title="バスケット"
-        padding="xl"
-        size="90%"
-        position="bottom"
+        title='バスケット'
+        classNames={{
+          header:
+            "box-content pb-4 border-b border-gray-400/40 mx-[-16px] px-[16px]",
+        }}
+        padding='md'
+        size='90%'
+        position='bottom'
         transitionDuration={150}
-        transitionTimingFunction="ease-in"
-        transition="slide-up"
+        transitionTimingFunction='ease-in'
+        transition='slide-up'
       >
         <Basket />
       </Drawer>
@@ -371,11 +459,20 @@ export default function SeatNo(props: any) {
 
 export async function getStaticProps() {
   // 商品一覧情報取得APIからメニューデータ取得
-  const { data } = await TableOrder().getItemData();
+  const docRef = doc(db, "TableOrderItemList", "0");
+  const data: any = (await getDoc(docRef))?.data();
+  console.log(data);
   const menuList = data?.items;
   const categoryName = data?.categoryName;
   // カテゴリー一覧取得APIからカテゴリー一覧取得
-  const categoryList = (await TableOrder().getCategoryData())?.data;
+  const citiesRef = collection(db, "TableOrderItemList");
+  const snapshot = await getDocs(citiesRef);
+  let categoryList = [];
+  snapshot.forEach((s) => {
+    const data = s.data();
+    delete data.items;
+    categoryList.push(data);
+  });
 
   return {
     props: {
