649d977ac9cc805882a6e16cf30de30db7c388d7
edegp <a.yuhi1164@gmail.com>
2022-09-10 13:55:47 +0900

    favicon setting

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -13,26 +13,28 @@ export default class _Document extends Document {
           <link
             rel='apple-touch-icon'
             sizes='180x180'
-            href='/favicon/apple-touch-icon.png'
+            href='/favicons/apple-touch-icon.png'
           />
           <link
             rel='icon'
             type='image/png'
-            href='/favicon/favicon-32x32.png'
             sizes='32x32'
+            href='/favicons/favicon-32x32.png'
           />
           <link
             rel='icon'
             type='image/png'
-            href='/favicon/favicon-16x16.png'
             sizes='16x16'
+            href='/favicons/favicon-16x16.png'
           />
-          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#ff4400' />
-          <meta name='theme-color' content='#fff' />
-          <meta
-            name='viewport'
-            content='minimum-scale=1, initial-scale=1, width=device-width'
+          <link rel='manifest' href='/favicons/site.webmanifest' />
+          <link
+            rel='mask-icon'
+            href='/favicons/safari-pinned-tab.svg'
+            color='#00ec00'
           />
+          <meta name='msapplication-TileColor' content='#00ec00' />
+          <meta name='theme-color' content='#00ec00' />
         </Head>
         <body>
           <Main />
