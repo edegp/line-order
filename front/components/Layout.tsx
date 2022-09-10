67a441ba280c68864acb8b3a6217a2229aa0e10e
edegp <a.yuhi1164@gmail.com>
2022-09-10 18:17:17 +0900

    change lorder to lorderOrverlay

--- front/components/Layout.tsx
+++ front/components/Layout.tsx
@@ -1,23 +1,25 @@
 import React, { ReactNode } from "react";
 import ErrorModal from "components/ErrorModal";
-import { Box, Loader } from "@mantine/core";
-import { useSelector } from "react-redux";
-import { State } from "types";
+import { Box, LoadingOverlay } from '@mantine/core'
+import { useSelector } from 'react-redux'
+import { State } from 'types'
 
 export default function Layout({ children }: { children: ReactNode }) {
-  const { lineUser, isLoading } = useSelector((state: State) => state);
-  const wrap = lineUser ? "wrap" : "hidden";
+  const { lineUser, isLoading } = useSelector((state: State) => state)
+  const wrap = lineUser ? 'wrap' : 'hidden'
   return (
     <>
-      {isLoading && (
-        <Box className='fixed inset-1/2'>
-          <Loader variant='bars' color='green.6' />
-        </Box>
-      )}
+      <Box className='fixed inset-1/2 bg-white'>
+        <LoadingOverlay
+          loaderProps={{ variant: 'bars', color: 'green.6' }}
+          visible={isLoading}
+          overlayBlur={2}
+        />
+      </Box>
       <div className={wrap}>
         {children}
         <ErrorModal />
       </div>
     </>
-  );
+  )
 }
